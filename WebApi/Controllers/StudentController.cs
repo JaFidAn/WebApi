e09cc490c03a9bcef468d90757615c6d8d6f364d
Rasim Alagezov <r.alagezov@gmail.com>
2022-05-04 17:00:06 +0400

    Add project files.

---
+++ WebApi/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private static List<Student> _students = new List<Student>();
+
+        [HttpGet("all")]
+        public List<Student> GetAll()
+        {
+            return _students;
+        }
+
+        [HttpPost("create")]
+        public Student CreateStudent(Student student)
+        {
+            _students.Add(student);
+            return student;
+        }
+
+        [HttpPut("update")]
+        public Student UpdateStudent(int id, Student newStudent)
+        {
+            var studend = _students.FirstOrDefault(student => student.Id == id);
+            studend.Name = newStudent.Name;
+            return newStudent;
+        }
+
+        [HttpDelete("delete")]
+        public Student DeleteStudent(int id)
+        {
+            var student = _students.FirstOrDefault(student => student.Id == id);
+            _students.Remove(student);
+            return student;
+        }
+    }
+}
